5b7a805bc16e0ec76d532991215a1cd2e101f587
nishitayalamanchili <nishita.yalamanchili@oneconvergence.com>
2016-04-22 19:39:44 -0700

    Added files via upload
    Demo scripts for austin workflow

---
+++ demo_scripts/demo_scripts/gbp_resource.sh
@@ -0,0 +1,171 @@
+#!/bash/bin
+dynamic=fw-lb
+image_name=cirros
+ext_segment=Datacenter-Out
+flav=m1.tiny
+function add_mem()
+{
+    image_id=$(openstack image list |grep  "$image_name" |awk '{print $2}'|head -n 1)
+    sleep 2
+    cptgid=$(gbp policy-target-group-list |grep -w $1  |awk '{print $2}')
+    i=1
+    while [ $i -le $2 ]
+    do
+        pt_id=$( gbp  pt-create --policy-target-group $cptgid $1"CPT"$i | grep  port_id | awk '{print $4 }' )
+        openstack server create --image $image_id --flavor $flav --nic port-id=$pt_id  $1"-MEM"$i
+        i=`expr $i + 1`
+    done
+}
+
+function del_mem()
+{
+
+    i=1
+    while [ $i -le $2 ]
+    do
+        sleep 2
+        gbp pt-delete $1"CPT"$i
+        i=`expr $i + 1`
+    done
+
+
+    i=1
+    while [ $i -le $2 ]
+    do
+        sleep 2
+        openstack server delete $1"-MEM"$i
+        i=`expr $i + 1`
+    done
+
+}
+
+function floatingipasso()
+{
+    a=$2
+    datacenter_name=$(neutron net-external-list  |grep Datacenter |awk '{print $4}')
+    floatingip_id=$( neutron  floatingip-create $datacenter_name |grep  id  |head -n 2 |tail -n 1  |awk '{print $4}')
+    echo $floatingip_id >>/tmp/floatingipidin$a
+    sleep 5
+    floating_ip=$(neutron floatingip-show $floatingip_id |grep  floating_ip_address |awk '{print $4}')
+    neutron  floatingip-associate $floatingip_id  $1
+
+}
+
+function floatingipdisasso()
+{
+    a=$1
+    filename=/tmp/floatingipidin$a
+    while read -r line
+    do
+        neutron floatingip-disassociate $line
+        sleep 5
+        neutron floatingip-delete $line
+        sleep 3
+
+    done < "$filename"
+}
+
+function  update()
+{
+
+    if [ "$dynamic" == "fw-lb" ]
+    then
+        vipid=$(neutron port-list |grep vip |awk '{print $2}')
+        echo -e $vipid
+        gbp policy-rule-set-update WEB-PRS --policy-rules "ICMPALLOW-PR TCPALLOW-PR WEB-PR "
+        vipid1=$(neutron port-list |grep vip|grep -v $vipid |awk '{print $2}')
+        echo -e $vipid1
+        floatingipasso $vipid1 $1
+    fi
+    if [ "$dynamic" == "fw" ]
+    then
+        gbp policy-target-group-update DB --provided-policy-rule-sets=TCP-PRS
+        #add_mem DB 1
+    fi
+}
+
+
+
+
+
+
+function dynamicinsertion()
+{
+    if [ "$dynamic" == "fw-lb" ]
+    then
+        echo -e "PRS update for fw-lb"
+        update $1 
+    fi
+
+    if [ "$dynamic" == "fw" ]
+    then
+        echo -e "PRS update for fw "
+        gbp policy-target-group-update DB --provided-policy-rule-sets=TCP-PRS
+    fi
+}
+
+
+function pre_create(){
+    action_name=$(gbp policy-action-list  |grep -w  FW-LB-REDIRECT |awk '{print $4}')
+    if [ "$action_name" == "FW-LB-REDIRECT" ]
+    then
+        echo -e "Delete previous gbp unshared resources which u created in tenant ."
+    else
+	gbp policy-action-create LB-REDIRECT --action-type redirect --action-value LB
+	gbp policy-action-create FW-REDIRECT --action-type redirect --action-value FW-2
+	gbp policy-action-create FW-LB-REDIRECT --action-type redirect --action-value FW-1-LB
+	gbp policy-action-create ALLOW --action-type allow
+	gbp policy-classifier-create HTTP-CLASSIFIER --protocol tcp --port-range 80 --direction bi
+	gbp policy-classifier-create TCP-CLASSIFIER --protocol tcp  --direction bi
+	gbp policy-classifier-create ICMP-CLASSIFIER --protocol icmp  --direction bi
+	gbp policy-rule-create HTTP-PR --classifier HTTP-CLASSIFIER --actions LB-REDIRECT
+	gbp policy-rule-create TCP-PR --classifier TCP-CLASSIFIER  --actions FW-REDIRECT
+	gbp policy-rule-create WEB-PR --classifier HTTP-CLASSIFIER --actions FW-LB-REDIRECT
+	gbp policy-rule-create  ICMPALLOW-PR --classifier ICMP-CLASSIFIER --actions ALLOW
+	gbp policy-rule-create TCPALLOW-PR --classifier TCP-CLASSIFIER --actions ALLOW
+	gbp policy-rule-set-create HTTP-PRS --policy-rules "ICMPALLOW-PR TCPALLOW-PR HTTP-PR"
+	gbp policy-rule-set-create TCP-PRS --policy-rules "ICMPALLOW-PR TCPALLOW-PR TCP-PR"
+	gbp policy-rule-set-create WEB-PRS --policy-rules "ICMPALLOW-PR TCPALLOW-PR"
+	gbp ptg-create DB
+	gbp ptg-create APP --network-service-policy LB-VIP-FIP-NSP
+	gbp ptg-create WEB  --network-service-policy  LB-VIP-FIP-NSP
+	gbp policy-target-group-update APP --provided-policy-rule-sets=HTTP-PRS  --consumed-policy-rule-sets TCP-PRS=None  
+	gbp policy-target-group-update WEB --provided-policy-rule-sets=WEB-PRS --consumed-policy-rule-sets HTTP-PRS=None 
+	gbp external-policy-create --external-segments $ext_segment  --consumed-policy-rule-sets WEB-PRS=None  EXTERNAL-WORLD
+	dynamicinsertion $1 
+
+	add_mem DB 1
+	add_mem APP 2
+	add_mem WEB 2
+
+    fi
+}
+
+function cleanup()
+{
+    floatingipdisasso $1
+    del_mem DB 1
+    del_mem APP 2
+    del_mem WEB 2
+    gbp policy-target-group-delete DB
+    gbp policy-target-group-delete APP 
+    gbp policy-target-group-delete WEB
+    gbp external-policy-delete EXTERNAL-WORLD
+    gbp policy-rule-set-delete HTTP-PRS
+    gbp policy-rule-set-delete TCP-PRS
+    gbp policy-rule-set-delete WEB-PRS
+    gbp policy-rule-delete HTTP-PR
+    gbp policy-rule-delete TCP-PR
+    gbp policy-rule-delete WEB-PR  
+    gbp policy-rule-delete ICMPALLOW-PR
+    gbp policy-rule-delete TCPALLOW-PR
+    gbp policy-classifier-delete HTTP-CLASSIFIER
+    gbp policy-classifier-delete TCP-CLASSIFIER
+    gbp policy-classifier-delete ICMP-CLASSIFIER 
+    gbp policy-action-delete LB-REDIRECT
+    gbp policy-action-delete FW-REDIRECT
+    gbp policy-action-delete FW-LB-REDIRECT
+    gbp policy-action-delete ALLOW
+}
+
+
---
+++ demo_scripts/demo_scripts/workflow.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+lb_prof=haproxy_lb
+#fw_prof=vyos_fw
+fw_prof=asav_fw
+
+function parallel()
+{
+    total=$1
+    slot=$2
+    if [ "$operation" ==  "create" ]
+    then
+        if [ "$slot" == "1" ]
+        then
+            for ((i=1; i<=total; i++)); do
+                for ((j=1; j<=slot; j++)); do
+                    source $PWD/keystone_directory/keystone-mem$i
+                    bash $PWD/operation.sh $3 $i
+                done
+            done
+        else
+            python $PWD/thread.py $1 $slot $operation
+        fi
+    fi
+    if [ "$operation" !=  "create" ]
+    then 
+        python $PWD/thread.py $1 $2 $operation
+    fi
+
+}
+function shared_validate()
+{
+    source $PWD/keystonerc_cloud_admin
+    node_name=$(gbp servicechain-node-list |grep -w LB |awk '{print $4}')
+    if [ "$node_name" == "LB" ]
+    then
+        echo -e "Create shared nodes , specs and nsp resource  in cloud_admin tenant ."
+    else
+	unset OS_USERNAME
+	unset OS_TENANT_NAME
+	unset OS_PASSWORD
+	unset OS_AUTH_URL
+	unset OS_REGION_NAME
+	tenant_create
+
+    fi
+}
+
+function tenant_create()
+{
+    echo -e "Enter number of tenant which u want at a  time in use"
+    read slot
+
+    echo -e "Enter number of tenant in multiple of " $slot
+    read n
+    echo $n
+    echo -e "Enter create or delete or update "
+    read operation
+    if (( $n %  $slot == 0 ))
+    then
+        if [ "$operation" ==  "create" ]
+        then
+            rm -rf /tmp/floatingipid*
+            mkdir $PWD/keystone_directory
+            source ./keystone_identity_v3
+            upperlim=$n
+            ten_count=1000
+            for ((i=1; i<=upperlim; i++)); do
+                p="TENANT$ten_count"
+
+                dom="default"
+                openstack project create --domain $dom $p
+                openstack user create --domain $dom --project $p --password $p $p
+                openstack role add --domain $dom --user $p domain_member
+                openstack role add --project $p --user $p _member_
+                rm -rf $PWD/keystone_directory/keystone-mem$i
+                echo -e "export OS_USERNAME=$p" >>$PWD/keystone_directory/keystone-mem$i
+                echo -e "export OS_PROJECT_NAME=$p">>$PWD/keystone_directory/keystone-mem$i
+                echo -e "export OS_TENANT_NAME=$p">>$PWD/keystone_directory/keystone-mem$i
+                echo -e "export OS_PASSWORD=$p">>$PWD/keystone_directory/keystone-mem$i
+                echo -e "export OS_AUTH_URL=http://10.251.1.20:5000/v3/">>$PWD/keystone_directory/keystone-mem$i
+
+                echo -e "export PS1='[\u@\h \W($p)]\$ '">>$PWD/keystone_directory/keystone-mem$i
+
+                echo -e "export OS_IDENTITY_API_VERSION=3">>$PWD/keystone_directory/keystone-mem$i
+                echo -e "export OS_USER_DOMAIN_NAME=$dom">>$PWD/keystone_directory/keystone-mem$i
+                echo -e "export OS_PROJECT_DOMAIN_NAME=$dom" >>$PWD/keystone_directory/keystone-mem$i
+                ten_count=`expr $ten_count + 17`
+            done
+
+            unset OS_USER_DOMAIN_NAME
+            unset OS_IDENTITY_API_VERSION
+            unset OS_DOMAIN_NAME
+            unset OS_USERNAME
+            unset OS_PASSWORD
+            unset OS_AUTH_URL
+            unset OS_REGION_NAME
+            sed -i "/dynamic=/c\dynamic=fw-lb" $PWD/gbp_resource.sh  
+            #sed -i "/dynamic=/c\dynamic=fw" $PWD/gbp_resource.sh
+            parallel $n $slot $operation
+        fi
+        if [ "$operation" == "update" ]
+        then
+            #sed -i "/dynamic=/c\dynamic=fw-lb" $PWD/gbp_resource.sh
+            sed -i "/dynamic=/c\dynamic=fw" $PWD/gbp_resource.sh
+            parallel $n $slot $operation 
+        fi
+        if [ "$operation" ==  "delete" ]
+        then
+            parallel $n $slot $operation
+            : '
+unset OS_PROJECT_NAME
+unset OS_USER_DOMAIN_NAME
+unset OS_IDENTITY_API_VERSION
+unset OS_PROJECT_DOMAIN_NAME
+unset OS_USERNAME
+unset OS_TENANT_NAME
+source ./keystone_identity_v3
+sleep 120
+
+for (( i=1; i<=$n; i++ ))
+do
+openstack user delete mem$i
+openstack project delete mem$i
+done
+rm -rf $PWD/keystone_directory 
+'
+        fi
+    else
+        echo -e $n " is not  multiple of " $slot
+    fi
+
+
+}
+#function shared_validate()
+#{
+#    source $PWD/keystonerc_cloud_admin
+#    node_name=$(gbp servicechain-node-list |grep -w LB |awk '{print $4}')
+#    if [ "$node_name" != "LB" ]
+#    then
+#        echo -e "Create shared nodes , specs and nsp resource  in cloud_admin tenant ."
+#    else
+#	unset OS_USERNAME
+#	unset OS_TENANT_NAME
+#	unset OS_PASSWORD
+#	unset OS_AUTH_URL
+#	unset OS_REGION_NAME
+#	tenant_create
+#
+#    fi
+#}
+
+function shared_resource()
+{
+    source $PWD/keystonerc_cloud_admin
+    echo -e "create for create or delete for delete operation"
+    read sop
+    if [ "$sop" != "create" -a "$sop" != "delete" ]
+    then
+        echo -e "Invalid argument : $1\n"
+        exit
+    fi
+    if [ "$sop" == "create" ]
+    then
+        node_name=$(gbp servicechain-node-list |grep -w LB |awk '{print $4}')
+        if [ "$node_name" == "LB" ]
+        then
+            echo -e "Delete previous shared node and spec and nsp resources which u created in cloud_admin tenant ."
+        else
+            
+            : '
+gbp service-profile-create --shared True --servicetype FIREWALL --insertion-mode l3 --service-flavor asav --vendor oneconvergence FW
+gbp service-profile-create --shared True --servicetype LOADBALANCER --insertion-mode l3 --service-flavor haproxy --vendor oneconvergence LB
+gbp external-segment-create --ip-version 4 --cidr 1.102.1.254/24 --external-route destination=0.0.0.0/0,nexthop= --shared True Management-Out
+gbp external-segment-create --ip-version 4 --cidr 1.102.2.254/24 --external-route destination=0.0.0.0/0,nexthop= --shared True default
+gbp nat-pool-create --ip-version 4 --ip-pool 1.103.2.0/24 --shared True --external-segment default default-nat-pool
+'
+            gbp servicechain-node-create --service-profile $fw_prof --shared True --template-file $PWD/fw.template1 FW-1
+            gbp servicechain-node-create --service-profile $fw_prof --shared True --template-file $PWD/fw.template2 FW-2
+            gbp servicechain-node-create --service-profile $lb_prof --shared True --template-file $PWD/lb.template  LB
+            gbp servicechain-spec-create LB --nodes  "LB"  --shared True
+            gbp servicechain-spec-create FW-2 --nodes  "FW-2"  --shared True
+            gbp servicechain-spec-create FW-1-LB --nodes  "FW-1 LB" --shared True
+            gbp network-service-policy-create LB-VIP-FIP-NSP --network-service-params "type=ip_single,name=vip_ip,value=self_subnet" --shared True
+
+            : '
+gbp policy-action-create LB-REDIRECT --action-type redirect --action-value LB --shared True
+gbp policy-action-create FW-REDIRECT --action-type redirect --action-value FW-2 --shared True
+gbp policy-action-create FW-LB-REDIRECT --action-type redirect --action-value FW-1-LB --shared True
+gbp policy-action-create ALLOW --action-type allow --shared True
+gbp policy-classifier-create HTTP-CLASSIFIER --protocol tcp --port-range 80 --direction bi --shared True
+gbp policy-classifier-create TCP-CLASSIFIER --protocol tcp  --direction bi --shared True
+gbp policy-classifier-create ICMP-CLASSIFIER --protocol icmp  --direction bi --shared True
+gbp policy-rule-create HTTP-ALLOW-PR --classifier HTTP-CLASSIFIER --actions ALLOW --shared True
+gbp policy-rule-create TCP-ALLOW-PR --classifier TCP-CLASSIFIER  --actions  ALLOW --shared True
+gbp policy-rule-create FW-LB-PR --classifier HTTP-CLASSIFIER --actions FW-LB-REDIRECT --shared True
+gbp policy-rule-create  LB-PR --classifier TCP-CLASSIFIER --actions LB-REDIRECT --shared True
+gbp policy-rule-create  FW-PR --classifier TCP-CLASSIFIER --actions FW-REDIRECT --shared True
+gbp policy-rule-set-create HTTP-ALLOW-PRS --policy-rules "HTTP-ALLOW-PR"  --shared True
+gbp policy-rule-set-create TCP-ALLOW-PRS --policy-rules "TCP-ALLOW-PR"  --shared True
+gbp policy-rule-set-create FW-LB-PRS --policy-rules "FW-LB-PR"  --shared True
+gbp policy-rule-set-create LB-PRS --policy-rules "LB-PR"  --shared True
+gbp policy-rule-set-create FW-PRS --policy-rules "FW-PR"  --shared True
+'
+        fi
+    fi
+    if [ "$sop" == "delete" ]
+    then
+        echo -e "These resources are shared resources before deleting make sure you deleted all resourecs from other tenants which used these shared resources.Press Y for Yes  or press N for No ."
+        read flag
+        if [ "$flag" != "Y" -a "$flag" != "N" ]
+        then
+            echo -e "Invalid argument : $1\n"
+            exit
+        fi
+        if [ "$flag" == "Y" ]
+        then
+            : '
+gbp external-segment-create --ip-version 4 --cidr 1.102.1.254/24 --external-route destination=0.0.0.0/0,nexthop= --shared True Management-Out
+gbp external-segment-create --ip-version 4 --cidr 1.102.2.254/24 --external-route destination=0.0.0.0/0,nexthop= --shared True default
+gbp nat-pool-create --ip-version 4 --ip-pool 1.103.2.0/24 --shared True --external-segment default default-nat-pool
+'
+            gbp network-service-policy-delete LB-VIP-FIP-NSP
+            gbp servicechain-spec-delete FW-1-LB
+            gbp servicechain-spec-delete FW-2 
+            gbp servicechain-spec-delete  LB 
+            gbp servicechain-node-delete FW-1
+            gbp servicechain-node-delete FW-2
+            gbp servicechain-node-delete LB 
+
+
+        fi
+        if [ "$flag" == "N" ]
+        then
+            echo -e " ok "
+        fi
+    fi
+
+}
+
+function start()
+{
+    echo -e "Enter operator or tenant ."
+    read op
+    if [ "$op" != "operator" -a "$op" != "tenant" ]
+    then
+        echo -e "Invalid argument : $1\n"
+        exit
+    fi
+
+    if [ "$op" == "operator" ]
+    then 
+        shared_resource
+    fi
+    if [ "$op" == "tenant" ]
+    then 
+        shared_validate
+    fi
+}
+
+start
+
+
+
